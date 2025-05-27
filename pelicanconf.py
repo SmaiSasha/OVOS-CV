44a58cd6f10930f2a806e6f8ab420fb147b536e4
SmaiSasha <124979493+SmaiSasha@users.noreply.github.com>
2025-05-27 19:15:30 +0300

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,28 @@
+AUTHOR = 'Aleksandr Smailov'
+SITENAME = 'ovoc_cv_pelican'
+SITEURL = 'https://smaisasha.github.io/OVOS-CV/'
+RELATIVE_URLS = False
+
+PATH = "content"
+
+TIMEZONE = 'W-SU'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
